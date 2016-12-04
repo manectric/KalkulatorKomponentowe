e2164236b37791f75d42e4011e32dc7681908f6c
Cezary <czrynkowski@gmail.com>
2016-12-04 11:30:54 +0100

    Init commit

---
+++ Kalkulator/IKalkulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    interface IKalkulator
+    {
+
+        double Dodaj(double a, double b);
+
+        double Odejmij(double a, double b);
+
+        double Pomnoz(double a, double b);
+
+        double Podziel(double a, double b);
+
+        double Pierwiastek(double a);
+
+    }
+}
---
+++ Kalkulator/Kalkulator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    public class Kalkulator : IKalkulator
+    {
+        public enum NazwaMetody
+        {
+            Dodawanie = 1,
+            Odejmowanie = 2,
+            Pomnoz = 3,
+            Podziel = 4,
+            Pierwiastek = 5
+        }
+
+        public double Dodaj(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Odejmij(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Pierwiastek(double a)
+        {
+            return Math.Sqrt(a);
+        }
+
+        public double Podziel(double a, double b)
+        {
+            return a/b;
+        }
+
+        public double Pomnoz(double a, double b)
+        {
+            return a*b;
+        }
+    }
+}
