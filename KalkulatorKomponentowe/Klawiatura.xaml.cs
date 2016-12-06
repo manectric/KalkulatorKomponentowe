f1a16abb7780e268b2a284aac52c5f505be01eff
Cezary <czrynkowski@gmail.com>
2016-12-06 23:27:24 +0100

    Fixed/Add UserControls

---
+++ KalkulatorKomponentowe/Klawiatura.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Kalkulator;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KalkulatorKomponentowe
+{
+
+    /// <summary>
+    /// Interaction logic for Klawiatura.xaml
+    /// </summary>
+    /// 
+    public partial class Klawiatura : UserControl, IKlawiatura
+    {
+        public Klawiatura()
+        {
+            InitializeComponent();
+        }
+
+        public string PobierzDane()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string WyslijDane()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        { 
+            //
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button_plus_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button_minus_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button_multiply_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button_divide_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button_square_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button_equal_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button0_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void button_clear_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+    }
+}
---
+++ KalkulatorKomponentowe/Wyswietlacz.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KalkulatorKomponentowe
+{
+    /// <summary>
+    /// Interaction logic for Wyswietlacz.xaml
+    /// </summary>
+    public partial class Wyswietlacz : UserControl, INotifyPropertyChanged
+    {
+        public Wyswietlacz()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void ChangeValue(string input, bool force = false)
+        {
+            if (force)
+                TextBlock_display.Text = "";
+            TextBlock_display.Text += input;
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeValue("");
+        }
+
+    }
+}
